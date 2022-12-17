56efb42f2b5b98fdd536c42168e3f3cc8ccb4c40
devarx-dev <devapriyareghu8@gmail.com>
2022-12-18 02:08:25 +0530

    Third Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+// import Calendar from 'rc-calendar';
+import React from 'react';
+import ColorSchemesExample from './components/ColorSchemesExample.js'
+import Calendar from './components/Calendar.js'
+// import ReactDOM from 'react-dom';
+// ReactDOM.render(, container);
+// import Calendar from "./components/Calendar"
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <ColorSchemesExample />
+      <Calendar />
         <h1>hello</h1>
       </header>
     </div>
